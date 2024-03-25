09e9942c6eaf23d57363357b01f0cdad8064f073
Agustin Arnaiz <arnaizagustin@gmail.com>
2024-03-25 12:15:14 -0300

    testimonios terminado

---
+++ freecodecamp-testimonios/src/componentes/Testimonio.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
+
+function Testimonio(props) {
+	return(
+		<article className='contenedor-testimonio'>
+			<img 
+				className= 'imagen-testimonio' 
+				src= {props.imagen}
+				title= {`foto de ${props.nombre}`} />
+			<div className='contenedor-texto-testimonio'>
+				<p className='nombre-testimonio'>
+					<b>{props.nombre}</b> en {props.pais}</p>
+				<p className='cargo-testimonio'>
+					{props.cargo} en <b>{props.empresa}</b></p>
+				<p className='texto-testimonio'>
+					{props.testimonio}</p>
+			</div>
+		</article>
+	);
+}
+
+export default Testimonio;
---
+++ freecodecamp-testimonios/src/componentes/Testimonios.jsx
@@ -0,0 +1,12 @@
+import img1 from '../imagenes/shawn.png';
+import img2 from '../imagenes/sarah.png';
+import img3 from '../imagenes/emma.png';
+import Testimonio from './Testimonio'
+
+function Testimonios() {
+    return(
+        <Testimonio></Testimonio>
+    );
+}
+
+export default Testimonios;
